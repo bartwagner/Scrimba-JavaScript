8b03e4d5faee8675c6acff0902eac4e15b6152f4
bartwagner <118787799+bartwagner@users.noreply.github.com>
2023-11-15 20:39:58 -0800

    points challenge

--- Class-03-VariablePractice/index.js
+++ Class-03-VariablePractice/index.js
@@ -8,7 +8,24 @@ let greeting = 'Hi there'
 function hello(){
     return greeting + ', ' + name + '!'
 }
-
-
 console.log(fullName)
 console.log(hello())
+
+let myPoints = 3
+
+function add3Points(){
+    myPoints += 3
+}
+
+function remove1Point(){
+    myPoints -= 1
+}
+
+add3Points()
+remove1Point()
+add3Points()
+remove1Point()
+add3Points()
+
+
+console.log(myPoints)
