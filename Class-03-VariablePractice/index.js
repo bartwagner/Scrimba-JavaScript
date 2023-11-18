092090d93ab8407c373bdf5cdad4b32774de50be
bartwagner <118787799+bartwagner@users.noreply.github.com>
2023-11-17 16:56:53 -0800

    show de results

--- Class-03-VariablePractice/index.js
+++ Class-03-VariablePractice/index.js
@@ -5,33 +5,35 @@ let fullName = firstName + ' ' + lastName
 document.getElementById("first_name").innerText = firstName
 document.getElementById("last_name").innerText = lastName
 document.getElementById("full_name").innerText = fullName
-
 document.getElementById("div").innerText = '------------------------------------------------------------------------'
 
-let name = 'Linda'
 let greeting = 'Hi there'
-
 function hello(){
-    return greeting + ', ' + name + '!'
+    return greeting + ', ' + firstName + '!'
 }
-console.log(fullName)
-console.log(hello())
+document.getElementById("greet").innerText = hello()
+document.getElementById("div2").innerText = '------------------------------------------------------------------------'
 
 let myPoints = 3
-
 function add3Points(){
     myPoints += 3
 }
-
 function remove1Point(){
     myPoints -= 1
 }
+calc()
+function calc(){
+    add3Points()
+    remove1Point()
+    add3Points()
+    remove1Point()
+    add3Points()
+}
+document.getElementById("calc").innerText = 'How much 3+3-1+3-1+3 = ' + myPoints
+document.getElementById("div3").innerText = '------------------------------------------------------------------------'
 
-add3Points()
-remove1Point()
-add3Points()
-remove1Point()
-add3Points()
+
+
 
 
 console.log(myPoints)
