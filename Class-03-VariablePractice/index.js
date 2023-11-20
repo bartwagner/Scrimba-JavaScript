d3518d116b881499eefc17490cc531ec11cd2a72
bartwagner <118787799+bartwagner@users.noreply.github.com>
2023-11-20 13:33:12 -0800

    new variables

--- Class-03-VariablePractice/index.js
+++ Class-03-VariablePractice/index.js
@@ -33,7 +33,9 @@ document.getElementById("calc").innerText = 'How much 3+3-1+3-1+3 = ' + myPoints
 document.getElementById("div3").innerText = '------------------------------------------------------------------------'
 
 
-
-
-
-console.log(myPoints)
+console.log("2" + 2) // "22"
+console.log(11 + 7) // 18
+console.log(6 + "5") // "65"
+console.log("My points: " + 5 + 9) // "My points: 59"
+console.log(2 + 2) //  4
+console.log("11" + "14") // "1114" 
