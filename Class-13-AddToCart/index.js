a077ba149ff1a7500d559607949ee8ae3e3de874
bartwagner <118787799+bartwagner@users.noreply.github.com>
2024-01-19 17:47:20 -0800

    created css template

--- Class-13-AddToCart/index.js
+++ Class-13-AddToCart/index.js
@@ -2,3 +2,11 @@
 Challenge:
 Make it so that when you click the 'Add to cart' button, whatever is written in the input field should be console logged.
 */
+
+const inputFieldEL = document.getElementById('input-field')
+const addButtonEl = document.getElementById("add-button")
+
+addButtonEl.addEventListener("click", function() {
+    let inputValue = inputFieldEL.value
+    console.log(inputValue)
+})
