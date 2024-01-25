3a00beef38e2894f56c5502873b0023fc865bfe3
bartwagner <118787799+bartwagner@users.noreply.github.com>
2024-01-24 19:23:57 -0800

    using the firebase
    Create a new app using the Firebase

--- Class-14-RealTime/index.js
+++ Class-14-RealTime/index.js
@@ -1,8 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://playground-db2c3-default-rtdb.firebaseio.com/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const moviesInDB = ref(database, "movies")
+
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
-    
-    console.log(`${inputValue} added to database`)
+    push(moviesInDB, inputValue)
 })
