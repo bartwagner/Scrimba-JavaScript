1d4cdb95361f88086d947f58a19ba9220995f746
bartwagner <118787799+bartwagner@users.noreply.github.com>
2024-01-25 14:52:24 -0800

    send the information
    I send the information to the firebase.

--- Class-13-AddToCart/index.js
+++ Class-13-AddToCart/index.js
@@ -1,12 +1,18 @@
-/*
-Challenge:
-Make it so that when you click the 'Add to cart' button, whatever is written in the input field should be console logged.
-*/
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from  "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSetting = {
+    databaseURL: "https://realtime-database-2ddc1-default-rtdb.firebaseio.com/"
+}
+
+const app = initializeApp(appSetting)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
 
 const inputFieldEL = document.getElementById('input-field')
 const addButtonEl = document.getElementById("add-button")
 
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEL.value
-    console.log(inputValue)
+    push(shoppingListInDB, inputValue)
 })
